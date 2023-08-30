3c9085d31a7a854093aaff816667f0877fbc8566
Breno Gomes da Fonseca <brenogfonseca@gmail.com>
2023-08-30 14:43:07 -0300

    theme color

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import ToggleTheme from "@/components/toggleTheme";
+
+export default function Dashboard() {
+  return (
+    <div>
+      Dashboard
+      <ToggleTheme />
+    </div>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { TextField, Button } from "@mui/material";
+import React from "react";
+
+export default function Login() {
+  return (
+    <div>
+      Login
+      <TextField />
+      <Button>Enviar</Button>
+    </div>
+  );
+}
